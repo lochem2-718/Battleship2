c42e7c680da8f98166b95420c67b253c1ef7fefe
Jared Weinberger <jjw376@nau.edu>
2018-05-11 22:09:29 -0700

    Add project files.

---
+++ Battleship/Game.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battleship
+{
+    class Game
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Battleship/Ship.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battleship
+{
+    class Ship
+    {
+        int health;
+        char direction;
+        int length;
+        int x;
+        int y;
+        public Ship(int x, int y, char direction)
+        {
+            this.x = x;
+            this.y = y;
+            this.direction = direction;
+        }
+    }
+}
